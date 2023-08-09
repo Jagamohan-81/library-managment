70aa311b6d1b18d409b4a6ec21f2d3d70a9b0a94
Jagamohan panda <jagamohan.panda@indusnet.co.in>
2023-08-09 11:44:01 +0530

    client setup done

--- server/index.js
+++ server/index.js
@@ -5,8 +5,11 @@ const userRouter = require("./routes/user.router");
 const app = express();
 app.use(express.json());
 
-app.use("/", userRouter);
+app.use("/user", userRouter);
 
+app.get("/", (req, res, next) => {
+  res.status(200).json("I am listing at 3001");
+});
 app.listen(3001, () => {
   console.log("Hi i am listening at port 3001");
 });
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -4,8 +4,12 @@ const { userRegister } = require("../controllers/userController");
 const { validateBody, schemas } = require("../helpers/bodyValidate");
 const { validateDB } = require("../helpers/userDbValidate");
 
+router.get("/", (req, res, next) => {
+  res.status(200).json({ message: "Welcome to user route" });
+});
+
 router.post(
-  "/",
+  "/register",
   validateBody(schemas.userRegistraionSchema),
   validateDB,
   userRegister
